d8fe5cb4adbbb682f0c6ffec935ce43c51d379e4
hanane <nam.hana@yahoo.fr>
2018-10-13 09:08:56 +0200

    header-php and footer-php modification

---
+++ footer.php
@@ -0,0 +1,33 @@
+  <!-- ==================start footer==================== -->
+  <footer>
+    <section>
+      <div class="desktop liens">
+        <div>
+          <a href="https://empreintesucree.fr/sujet/entremets/"><img class="lien" src="img/lien1.png" alt="image gateau"></a>
+          <a href="http://www.pourcel-chefs-blog.com/blog1/2013/05/31/fruits-rouges-pour-la-collection-patisserie-2013-pour-cyril-lignac/"><img class="lien" src="img/lien2.jpg" alt="image gateau"></a>
+          <a href="http://patisserie-boulangerie.vip-blog.com/vip/categories/68939.html"><img class="lien" src="img/lien3.JPG" alt="image gateau"></a>
+        </div>
+        <div>
+          <a href="https://www.lecoconutblog.com/2018/04/fraisier-patissier-tout-tout-tout-fait-maison/"><img class="lien" src="img/lien4.jpg" alt="image gateau"></a>
+          <a href="https://www.lilleenbouche.fr/recettes/patisserie/gateaux-biscuits/entremet-peche-fruits-rouges"><img class="lien" src="img/lien5.jpg" alt="image gateau"></a>
+          <a href="https://blog.cerfdellier.com/realisez-de-delicieux-entremets-avec-le-cercle-a-patisserie-de-buyer/"><img class="lien" src="img/lien6.jpg" alt="image gateau"></a>
+        </div>
+        <div>
+          <a href="http://www.lacuisinedebernard.com/2011/07/le-flan-parisien.html"><img class="lien" src="img/lien7.jpg" alt="image gateau"></a>
+          <a href="https://www.cuisineaz.com/recettes/zlabia-boufarik-88785.aspx"><img class="lien" src="img/lien8.jpg" alt="image gateau"></a>
+          <a href="https://aupaysdelilie.com/2018/04/25/des-cupcakes-licorne-a-la-myrtille-%f0%9f%a6%84/"><img class="lien" src="img/lien9.png" alt="image gateau"></a>
+        </div>
+      </div>
+      <!-- ===footer end=== -->
+      <h5>Réalisé par Hanane</h5>
+      <h5><a href="https://www.adep-roubaix.fr/">ADEP de Roubaix</a></h5>
+      <div class="flex">
+        <p class="iconReseau"><a href="https://www.facebook.com/"><i class="fab fa-facebook-square"></i></a></p>
+        <p class="iconReseau"><a href="https://www.linkedin.com/feed/"><i class="fab fa-linkedin"></i></a></p>
+        <p class="iconReseau"><a href="https://www.github.com/"><i class="fab fa-github-square"></i></a></p>
+        <p class="iconReseau"><a href="https://slack.com/signin"><i class="fab fa-slack"></i></a></p>
+        <p class="iconReseau"><a href="https://twitter.com/"><i class="fab fa-twitter-square"></i></a></p>
+      </div>
+    </section>
+  </footer>
+  <!-- ==================end footer==================== -->
